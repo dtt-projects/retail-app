d01f1d68d62448698d273a135053f94bf9b72fd4
Oakley <matthew.oakley1@marist.edu>
2019-11-02 21:40:19 -0400

    Fixed up the cookie file

--- scripts/cookie-helper.js
+++ scripts/cookie-helper.js
@@ -1,9 +1,21 @@
+/**
+ * @module scripts/cookie-helper.js
+ * @fileoverview A helper file for handling cookies
+ * @exports handleLoginCookie handles cookies for logging in
+ * @exports handleCreateAccountCookie handles cookies for new accounts
+ * @exports handleNormalPageCookie handles cookies for all other pages
+ */
 
 // This helper file makes a cleaner reading of a credientials file for
 // hidden information like DB credientials
 var hidden = require('./read-hidden.js');
 
-// checks to see if a cookie is valid
+/**
+ * @function checkCookie
+ * @description Takes in a cookie and compares it to database values to
+ *    determine if it is valid or not.
+ * @param cookie the cookie that is being checked
+ */
 function checkCookie(cookie) {
   return new Promise(function(resolve, reject) {
     // get the creds from the hidden file
@@ -27,18 +39,24 @@ function checkCookie(cookie) {
 
         statement_0 = ("SELECT CURRENT_TIMESTAMP");
         con.query(statement_0, function(err_0, result_0) {
+          // value on getting timestamp so return a null for cookie
           if (err_0) {
+            // close db connection and send error to console
             con.end();
             console.log(err_0);
             resolve(null);
+          // timestamp from db and will be used for comparing for timeout
           } else if (result_0.length > 0) {
             // cookie has been inactive for 30 mins
-            if (new Date(result_0[0]["CURRENT_TIMESTAMP"]) - new Date(cookie["last_seen"]) > 1800000) {
-              console.log("INVALID");
+            var thirtyMins = 1800000;
+            if (new Date(result_0[0]["CURRENT_TIMESTAMP"])
+                - new Date(cookie["last_seen"]) > thirtyMins) {
+              // log cookie timeout, close connection, and send null for cookie
+              console.log("Cookie Timeout!");
               con.end();
               resolve(null);
+            // within 30 mins of activity so check it is a valid cookie
             } else {
-
               // this is to see if cookieID is real and get corrresponding aid
               statement = ("SELECT accountID FROM cookies WHERE cookieID='"
                   + cookie["cookieId"] + "' AND last_seen='"
@@ -46,35 +64,46 @@ function checkCookie(cookie) {
 
               // run the statement on the db
               con.query(statement, function(err, result) {
+                // error so end connection, log it, and send null for cookie
                 if (err) {
                   con.end();
                   console.log(err);
                   resolve(null);
+                // we got results so the statement was valid
                 } else if (result.length > 0) {
                   var aid = result[0]["accountID"];
-                  // validate the data of the cookie
+                  // validate the rest of the data from the cookie
                   statement_1 = ("SELECT isAdmin FROM accounts "
                       + "where aid='" + aid + "' AND "
                       + "username='" + cookie["username"] + "' AND "
                       + "email='" + cookie["email"] + "'");
                   // run the statement on the db
                   con.query(statement, function(err_1, result_1) {
+                    // we got an error so end conection, log it, and send back
+                    // null for the cookie
                     if (err_1) {
                       console.log(err_1);
                       con.end();
                       resolve(null);
+                    // we got back valid results from the db
                     } else if (result_1.length > 0) {
-                      // validate creds from cookie
+                      // validate creds from cookie so we end connection
                       con.end();
+                      // now we will update the value of the cookie
                       resolve(updateCookie(cookie, aid));
+                    // it didnt match up with the rest of the cookie database
+                    // so we will log it, end connection and send back null
                     } else {
+                      console.log("INVALID Cookie: full check!")
                       con.end();
                       resolve(null);
                     }
                   });
-                // invalid cookie
+                // cookie id and timestamp pair didnt exist so its invalid
+                // send back null because of invalid cookie
                 } else {
                   con.end();
+                  console.log("INVALID Cookie: cid and last_seen invalid!")
                   resolve(null);
                 }
               });
@@ -85,6 +114,12 @@ function checkCookie(cookie) {
   })
 }
 
+/**
+ * @function createCookie
+ * @description Takes in a cookie and compares it to database values to
+ *    determine if it is valid or not.
+ * @param user_info the cookie that is being checked
+ */
 function createCookie(user_info) {
   return new Promise(function(resolve, reject) {
     // get the creds from the hidden file
@@ -191,6 +226,7 @@ function createCookie(user_info) {
               }
             })
           } else {
+            console.log("Cookies problem with getting user information");
             con.end();
             resolve(null);
           }
@@ -199,6 +235,12 @@ function createCookie(user_info) {
   });
 }
 
+/**
+ * @function updateCookie
+ * @description Takes in a cookie and aid and updates it in the cookie table
+ * @param cookie the cookie that is being checked
+ * @param aid the users related accountId
+ */
 function updateCookie(cookie, aid) {
   return new Promise(function(resolve, reject) {
     // get the creds from the hidden file
@@ -221,24 +263,29 @@ function updateCookie(cookie, aid) {
           }
         });
 
+        // update the table with the latest timestamp
         statement_1 = ("UPDATE cookies "
             + "set last_seen=CURRENT_TIMESTAMP "
             + "where cookieID='" + cookie["cookieId"] + "'");
         con.query(statement_1, function(err_1, result_1) {
+          // failure means log, end connection, and send back null
           if (err_1) {
             console.log(err_1);
             con.end();
             resolve(null);
+          // if it doesnt error then setup the new cookie
           } else {
             // getting the cookieId so it can be referenced in the future
             statement_2 = ("SELECT cookieID, last_seen FROM cookies "
                 + "WHERE accountID='" + aid + "'");
             con.query(statement_2, function(err_2, result_2) {
+              // if errors then log, end conenction, and send null cookie
               if (err_2) {
                 console.log(err_2);
                 con.end();
                 resolve(null);
-              } else {
+              // we got a response back with information so we can continue
+              } else if (result_2.length > 0) {
                 var new_cookie = {"cookieId": result_2[0]["cookieID"],
                                   "email": cookie["email"],
                                   "username": cookie["username"],
@@ -247,6 +294,11 @@ function updateCookie(cookie, aid) {
                             };
                 con.end();
                 resolve(new_cookie);
+              // no data back so we return null
+              } else {
+                con.end();
+                console.log("Error with getting latest cookie");
+                resolve(null);
               }
           });
         }
@@ -255,29 +307,46 @@ function updateCookie(cookie, aid) {
   })
 }
 
-// handles everything to do with a cookie
-exports.handleLoginCookie = function(cookie, user_info) {
+/**
+ * @function handleLoginCookie
+ * @description Takes in a cookie and user info to check if a login was validate
+ *    if the user logs in they will get back a proper cookie
+ * @param cookie the cookie that is being checked
+ * @param userInfo the info submitted from the login page
+ */
+exports.handleLoginCookie = function(cookie, userInfo) {
   return new Promise(function(resolve, reject) {
-    if (cookie == "undefined" || cookie == null) {
+    // check if the cookie passed in is valid or not
+    try {
+      if (cookie == "undefined" || cookie == null) {
+        cookie = null;
+      } else if (cookie["CID"] == "undefined" || cookie["CID"] == null) {
+        cookie = null;
+      }
+    } catch(e) {
+      console.log("Error with handleLoginCookie: " + e);
+      // cookie was not valid so set it to null
       cookie = null;
-    } else if (cookie["CID"] == "undefined" || cookie["CID"] == null) {
-      cookie = null;
-    }
-
-    // cookie exists check if valid
-    if (cookie != null) {
-      // check if it is a valid cookie
-      resolve(checkCookie(cookie["CIS"], user_info));
+      // cookie exists check if valid
+      if (cookie != null) {
+        // check if it is a valid cookie
+        resolve(checkCookie(cookie["CID"], userInfo));
       // create a cookie
-    } else {
-      resolve(createCookie(user_info));
+      } else {
+        resolve(createCookie(userInfo));
+      }
     }
   });
 }
 
-exports.handleCreateAccountCookie = function(user_info) {
+/**
+ * @function handleCreateAccountCookie
+ * @description Takes in user info and builds a new cookie out of it for the DB
+ * @param userInfo what was passed in on account creation
+ */
+exports.handleCreateAccountCookie = function(userInfo) {
   return new Promise(function(resolve, reject) {
-    resolve(createCookie(user_info));
+    resolve(createCookie(userInfo));
   });
 }
 
@@ -292,10 +361,10 @@ exports.handleNormalPageCookie = function(cookie) {
     // check if a cokie is present
     try {
       // no cookies exist at all
-      if (cookie == null || cookie == "undefined") {
+      if (cookie == "undefined" || cookie == null) {
         resolve(null);
       // cookie for CID doesnt exists but other may exist
-      } else if (cookie["CID"] == null || cookie["CID"] == "undefined") {
+      } else if (cookie["CID"] == "undefined"|| cookie["CID"] == null) {
         resolve(null);
       // check to see if cookie is valid
       } else {
