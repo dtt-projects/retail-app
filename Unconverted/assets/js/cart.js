bbc48b83f965e3bf3772ede53fc4262f929ce4b7
AlexMahl <mattmahlmeister@Alex-Mahlmeisters-MacBook-Pro-2297.local>
2019-11-17 19:07:33 -0500

    Market Place
    Adds Market Place and corresponding pages

---
+++ Unconverted/assets/js/cart.js
@@ -0,0 +1,7 @@
+document.getElementById('addToCart').onclick = function(){
+    alert("Added To Cart");
+}
+
+document.getElementById('addToCart1').onclick = function(){
+    alert("Added To Cart1");
+}
