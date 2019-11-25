3ff359a961d33822010d5e159c6bcabfe4fa79c3
AlexMahl <mattmahlmeister@148-100-138-63.FoxNet.marist.edu>
2019-11-25 16:55:38 -0500

    Adds New Pages / Updates Functionality
    Adds User and Admin section
    Fixes CSS issues

---
+++ Unconverted/assets/js/editAccount.js
@@ -0,0 +1,32 @@
+$("#editAccountInformation").click(function(){
+    
+    document.getElementById('sumbitChanges').style.display = "block";
+    document.getElementById('cancelChanges').style.display = "block";
+    document.getElementById('editAccountInformation').style.display = "none";
+    
+    document.getElementById('firstName').contentEditable = "true";
+    document.getElementById('lastName').contentEditable = "true";
+    document.getElementById('phoneNumberHome').contentEditable = "true";
+    document.getElementById('phoneNumberMobile').contentEditable = "true";
+    document.getElementById('addressOne').contentEditable = "true";
+    document.getElementById('addressTwo').contentEditable = "true";
+    document.getElementById('cityName').contentEditable = "true";
+    document.getElementById('cityName').contentEditable = "true";
+    document.getElementById('zipCode').contentEditable = "true";
+    document.getElementById('genderCode').contentEditable = "true";
+    
+    $('html, body').animate({ scrollTop: 0 }, 'slow');
+});
+
+$("#sumbitChanges").click(function(){
+    location.reload();
+    $('html, body').animate({ scrollTop: 0 }, 'slow');
+});
+
+
+
+$("#cancelChanges").click(function(){
+    location.reload();
+    $('html, body').animate({ scrollTop: 0 }, 'slow');
+});
+
